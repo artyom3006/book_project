2285b4101f2881d008cd643a0c6a51ec83ce0cce
Artyom <artyomshekhonin@gmail.com>
2023-12-25 00:07:42 +0300

    ch12.1

--- books/views.py
+++ books/views.py
@@ -1,4 +1,7 @@
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
+from .forms import BookForm
+from django.urls import reverse_lazy
 
 from .models import Book
 
@@ -11,3 +14,9 @@ class BookDetailView(DetailView):
     model = Book
     template_name = 'books/book_detail.html'
     context_object_name = 'book'
+    
+class BookCreateView(CreateView):
+    model = Book
+    form_class = BookForm
+    template_name = 'books/book_form.html'
+    success_url = reverse_lazy('book_list')
