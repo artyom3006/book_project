bd722f4359f8bf7ac8eb9c1d6be6e96f8d5d797a
Artyom <artyomshekhonin@gmail.com>
2024-01-05 00:06:15 +0300

    ch18+allowed_host_glitch.me

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1', 'artyom3006.pythonanywhere.com']
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1', 'artyom3006.pythonanywhere.com' '.glitch.me']
 
 
 # Application definition
